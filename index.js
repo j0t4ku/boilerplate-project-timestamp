c685b17b0a45d1e66720199f21597308d4563d04
j0t4ku <j.ismar00@gmail.com>
2023-03-01 11:15:02 -0300

    Modify api/ for  void parameter

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get("/api/:date?",(req,res)=>{
+app.get("/api/:date",(req,res)=>{
   let unixTimestamp;
   let utcDate;
   res.set({'Content-Type':'application/json'})
